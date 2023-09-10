138508e149c7f12685e2ca4a7081c95301fd9a60
SIH CI ZENG <aa51223tw28@gmail.com>
2023-09-10 21:42:47 +0800

    投票票數清單頁ok

--- online-voting-system0910/Controllers/ForestageController.cs
+++ online-voting-system0910/Controllers/ForestageController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using online_voting_system0910.Models;
+using online_voting_system0910.Models.ViewModel;
 
 namespace online_voting_system0910.Controllers
 {
     public class ForestageController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly voting0910Context _Context;
+
+        public ForestageController(ILogger<HomeController> logger, voting0910Context Context)
+        {
+            _logger = logger;
+            _Context = Context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var votingRecords = _Context.VotingRecord.ToList();
+            var votingItems = _Context.VotingItem.ToList();
+
+            var viewModel = from item in votingItems
+                            join record in votingRecords on item.VotingItemId equals record.VotingItemId into recordGroup
+                            select new VotingViewModel
+                            {
+                                VotingItemId = item.VotingItemId,
+                                VotingItemName = item.VotingItemName,
+                                VotingCount = recordGroup.Sum(r => 1)
+                            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ online-voting-system0910/Models/ViewModel/VotingViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace online_voting_system0910.Models.ViewModel
+{
+    public class VotingViewModel
+    {
+        public int VotingItemId { get; set; }
+        public string VotingItemName { get; set; }
+        public int VotingCount { get; set; }
+    }
+}
