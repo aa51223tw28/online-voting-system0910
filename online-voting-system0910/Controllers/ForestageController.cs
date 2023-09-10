faaa2cb0fc62c95c02ccaf460cb6d35284de2c3f
SIH CI ZENG <aa51223tw28@gmail.com>
2023-09-10 22:25:05 +0800

    投票ok

--- online-voting-system0910/Controllers/ForestageController.cs
+++ online-voting-system0910/Controllers/ForestageController.cs
@@ -18,6 +18,13 @@ namespace online_voting_system0910.Controllers
 
         public IActionResult Index()
         {
+            var viewModel = BuildVotingViewModel();
+            return View(viewModel);
+        }
+
+
+        private List<VotingViewModel> BuildVotingViewModel()
+        {
             var votingRecords = _Context.VotingRecord.ToList();
             var votingItems = _Context.VotingItem.ToList();
 
@@ -30,13 +37,40 @@ namespace online_voting_system0910.Controllers
                                 VotingCount = recordGroup.Sum(r => 1)
                             };
 
-            return View(viewModel);
+            return viewModel.ToList();
         }
+
+
         public IActionResult Create()
         {
             var votingItems = _Context.VotingItem.ToList();
             ViewBag.VotingItems = votingItems;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VotingCreateViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                foreach (var votingItemId in viewModel.SelectedVotingItems)
+                {
+                    var votingItemRecord = new VotingRecord
+                    {
+                        Voter = viewModel.Voter,                       
+                        VotingItemId = votingItemId
+                    };
+                    _Context.VotingRecord.Add(votingItemRecord);
+                }
+
+                _Context.SaveChanges();
+
+                return RedirectToAction("Index"); 
+            }
+
+            return RedirectToAction("Index");
+            
         }
     }
 }
---
+++ online-voting-system0910/Models/ViewModel/VotingCreateViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace online_voting_system0910.Models.ViewModel
+{
+    public class VotingCreateViewModel
+    {
+        public int VotingRecordId { get; set; }
+        public string Voter { get; set; }
+        public List<int> SelectedVotingItems { get; set; }
+    }
+}
