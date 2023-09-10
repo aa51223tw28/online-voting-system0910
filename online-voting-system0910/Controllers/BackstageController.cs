1418a80d9382448b827c275a2ab237bf76ac6e39
SIH CI ZENG <aa51223tw28@gmail.com>
2023-09-10 21:19:00 +0800

    後台編輯ok

--- online-voting-system0910/Controllers/BackstageController.cs
+++ online-voting-system0910/Controllers/BackstageController.cs
@@ -37,5 +37,23 @@ namespace online_voting_system0910.Controllers
             _Context.SaveChanges();
             return RedirectToAction("Index");
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+            VotingItem prod = _Context.VotingItem.FirstOrDefault(p => p.VotingItemId == id);        
+            return View(prod);            
+        }
+        [HttpPost]
+        public IActionResult Edit(VotingItem item)
+        {
+            VotingItem prod = _Context.VotingItem.FirstOrDefault(p => p.VotingItemId == item.VotingItemId);
+            if (prod != null)
+            {                
+                prod.VotingItemName = item.VotingItemName;
+                _Context.SaveChanges();
+            }
+            return RedirectToAction("Index");          
+        }
     }
 }
