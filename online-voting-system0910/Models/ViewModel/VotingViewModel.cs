64d603104386c339600ae7348b1713a679027f1e
SIH CI ZENG <aa51223tw28@gmail.com>
2023-09-10 21:46:47 +0800

    VotingViewModel增加DisplayName

--- online-voting-system0910/Models/ViewModel/VotingViewModel.cs
+++ online-voting-system0910/Models/ViewModel/VotingViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace online_voting_system0910.Models.ViewModel
+﻿using System.ComponentModel;
+
+namespace online_voting_system0910.Models.ViewModel
 {
     public class VotingViewModel
     {
         public int VotingItemId { get; set; }
+        [DisplayName("投票項目名稱")]
         public string VotingItemName { get; set; }
+        [DisplayName("投票項目票數")]
         public int VotingCount { get; set; }
     }
 }
